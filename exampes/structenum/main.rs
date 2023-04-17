bdac61acc0a36889594b92a8d10df70aed9128b4
forfd8960 <forfd8960@gmail.com>
2023-04-17 22:00:23 +0800

    Impl method for Enum

--- exampes/structenum/main.rs
+++ exampes/structenum/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+enum Lang {
+    Rust,
+    Go,
+    Java,
+    TypeScript,
+}
+
+impl Lang {
+    fn to_string(&self) -> String {
+        match self {
+            Lang::Rust => "with Rust".to_string(),
+            Lang::Go => "with Golang".to_string(),
+            Lang::Java => "with Java".to_string(),
+            Lang::TypeScript => "with TS".to_string(),
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Date {
@@ -7,20 +25,37 @@ struct Date {
 }
 
 #[derive(Debug)]
+struct LangList {
+    lang_list: Vec<Lang>
+}
+
+impl LangList {
+    fn push(&mut self, lang: Lang) {
+        self.lang_list.push(lang)
+    }
+
+    fn list_langs(&self, result: &mut Vec<String>) {
+        for l in &self.lang_list {
+            result.push(l.to_string())
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Project<'a> {
     name: &'a str,
-    start_date: Date,
-    language: Vec<&'a str>,
+    start_date: &'a Date,
+    language: &'a mut LangList,
     code_lines: u32,
     eng_count: u8,
 }
 
 impl<'a> Project<'a> {
-    fn new(name: &'a str, start_date: Date, langage: Vec<&'a str>) -> Self {
+    fn new(name: &'a str, start_date: &'a Date, langages: &'a mut LangList) -> Self {
         Project {
-            name: name, 
-            start_date: start_date, 
-            language: langage,
+            name: name,
+            start_date: start_date,
+            language: langages,
             code_lines: 0,
             eng_count: 0,
         }
@@ -34,38 +69,41 @@ impl<'a> Project<'a> {
         self.eng_count += count;
     }
 
-    fn append_lang(&mut self, new_lang: &'a str) {
+    fn append_lang(&mut self, new_lang: Lang) {
         self.language.push(new_lang);
+    }
+
+    fn list_langs(&self, result: &mut Vec<String>) {
+       self.language.list_langs(result)
     }
 }
 
 fn main() {
-    let date = Date{
+    let date = Date {
         year: 2023,
         month: 4,
         day: 17,
     };
     let name = "rust-project";
-    let lang = vec!["rust"];
-    let prj = Project::new(&name, date, lang);
+    let lang = &mut LangList { lang_list: vec![Lang::Rust] };
+    let prj = Project::new(&name, &date, lang);
     println!("{:?}", prj);
 
-    let date2 = Date{
-        year: 2023,
-        month: 4,
-        day: 17,
-    };
     let name = "another-project";
-    let lang = vec!["rust"];
-    let mut prj2 = Project::new(&name, date2, lang);
+    let mut prj2 = Project::new(&name, &date, lang);
     println!("{:?}", prj2);
 
-    prj2.append_lang("go");
-    prj2.append_lang("js");
+    prj2.append_lang(Lang::Go);
+    prj2.append_lang(Lang::TypeScript);
+    prj2.append_lang(Lang::Java);
     println!("{:?}", prj2);
 
     prj2.increase_lines(100);
     println!("code_lines: {}", prj2.code_lines);
     prj2.increase_lines(1000);
     println!("code_lines: {}", prj2.code_lines);
+
+    let result = &mut Vec::<String>::new();
+    prj2.list_langs(result);
+    println!("{:?}", result);
 }
