b80587d1dc2516d85ec99bd8889c1b48a6d939d0
forfd8960 <forfd8960@gmail.com>
2023-04-17 09:47:31 +0800

    Match Expression

--- exampes/controlflow/main.rs
+++ exampes/controlflow/main.rs
@@ -2,6 +2,13 @@
     if_else_if();
     match_expr();
     get_num_from_match();
+    match_tuple();
+    match_guard();
+    match_part_value((12, 0, 0));
+    match_part_value((9, 59, 0));
+    match_part_value((23, 59, 0));
+    match_part_value((10, 0, 59));
+    match_part_value((10, 6, 30));
  }
 
  fn if_else_if() {
@@ -35,4 +42,43 @@
         _ => -1,
     };
     println!("output_num is: {}", output_num);
+ }
+
+ fn match_tuple() {
+    let sky = "cloudy";
+    let temp = "warm";
+    match (sky, temp) {
+        ("cloudy", "warm") => println!("oh warm cloudy day"),
+        ("clear", "warm") => println!("nice day"),
+        _ => println!("what day is it today"),
+    }
+ }
+
+ fn match_guard() {
+    let day = 30;
+    let lines = 1000;
+    match (day, lines) {
+        (day, lines) => { 
+            if day == 30 || day == 31 {
+                println!("oh seems last day")
+            }
+            if lines > 900 {
+                println!("productive day");
+            }
+        },
+    }
+ }
+
+ fn match_part_value(time: (u8, u8, u8)) {
+    match time {
+        (hour, _, _,) if hour == 12 => {println!("time for lunch")},
+        (hour, minute, _,) if hour == 23 && minute == 59 => {
+            println!("another day gone");
+        },
+        (_, minute, _) if minute == 59 => println!("another hour gone"),
+        (_, _, second) if second == 59 => println!("another minute gone"),
+        t => {
+            println!("{:?}", t);
+        }
+    }
  }
