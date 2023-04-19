4724338a4e43ff1da5e6c36b709cfedc4db2ca89
forfd8960 <forfd8960@gmail.com>
2023-04-19 21:05:13 +0800

    Impl custom trait for std data type

--- exampes/trait/main.rs
+++ exampes/trait/main.rs
@@ -1,5 +1,26 @@
 use std::convert::From;
 
+trait Concat<T> {
+    type Output;
+    fn concat(&self, r: T) -> Self::Output
+    where
+        T: ToString;
+}
+
+impl Concat<i32> for i32 {
+    type Output = String;
+    fn concat(&self, r: i32) -> Self::Output {
+        format!("{} - {}", self.to_string(), r.to_string())
+    }
+}
+
+impl Concat<&str> for &str {
+    type Output = String;
+    fn concat(&self, r: &str) -> Self::Output {
+        format!("{} - {}", self.to_string(), r.to_string())
+    }
+}
+
 #[derive(Debug)]
 struct Data {
     name: String,
@@ -19,9 +40,22 @@ impl From<&str> for Data {
 }
 
 fn main() {
+    // Implement From trait for Data
     let data = Data::from("wiki,this is a wiki,90");
     println!("{:?}", data);
     println!("name: {}", data.name);
     println!("desc: {}", data.desc);
     println!("view: {}", data.value);
+
+    // Implement Concat trait for i32 and &str
+    println!("-------Implement Concat trait for i32 and &str--------");
+    let my_str = "Hello";
+    let my_str1 = "Dear";
+    let concat_str = my_str.concat(my_str1);
+    println!("concat str: {}", concat_str);
+
+    let my_num = 999;
+    let my_num2 = 688;
+    let concat_num = my_num.concat(my_num2);
+    println!("concated number: {}", concat_num);
 }
