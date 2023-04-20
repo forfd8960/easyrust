df7a3493ff928d8e457b9453ab4c022b2c40b885
forfd8960 <forfd8960@gmail.com>
2023-04-20 08:41:36 +0800

    Implement Iterator for Struct

--- exampes/iterator/main.rs
+++ exampes/iterator/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+enum Programmer {
+    Rust,
+    Go,
+    Js,
+    Python,
+}
+
+struct ItCompany {
+    name: String,
+    programmers: Vec<Programmer>,
+}
+
+impl Iterator for ItCompany {
+    type Item = Programmer;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.programmers.pop()
+    }
+}
+
 fn main() {
     let origin = vec![1, 6, 8];
 
@@ -13,6 +33,8 @@ fn main() {
 
     run_iter();
     run_into_iter();
+    iter_next();
+    iter_over_company();
 }
 
 fn run_iter() {
@@ -37,5 +59,39 @@ fn run_into_iter() {
 
     for n in vec1 {
         println!("{}", n);
+    }
+}
+
+/*
+    An iterator works by using a method called .next(),
+    which gives an Option. When you use an iterator,
+    Rust calls next() over and over again.
+    If it gets Some, it keeps going. If it gets None, it stops.
+*/
+fn iter_next() {
+    let words = vec!["this", "is", "a", "good", "day"];
+    let mut my_iter = words.iter();
+
+    // next returns refer of the value in my_iter
+    assert_eq!(my_iter.next(), Some(&"this"));
+    assert_eq!(my_iter.next(), Some(&"is"));
+    assert_eq!(my_iter.next(), Some(&"a"));
+    assert_eq!(my_iter.next(), Some(&"good"));
+
+    /*thread 'main' panicked at 'assertion failed: `(left == right)`
+     left: `Some("day")`,
+    right: `Some("da")`', iterator/main.rs:53:5*/
+    assert_eq!(my_iter.next(), Some(&"day"));
+    assert_eq!(my_iter.next(), None);
+}
+
+fn iter_over_company() {
+    let cmy = &mut ItCompany {
+        name: "A SAAS startup".to_string(),
+        programmers: vec![Programmer::Rust, Programmer::Go],
+    };
+
+    for programmer in cmy {
+        println!("{:?}", programmer);
     }
 }
