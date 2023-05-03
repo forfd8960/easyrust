20ca3b22cfc4fe6e7db08645e1f4ed79eee1f74c
forfd8960 <forfd8960@gmail.com>
2023-05-03 12:16:17 +0800

    Rc - Use Rc::clone to link list

--- examples/rc/cons.rs
+++ examples/rc/cons.rs
@@ -1,15 +1,30 @@
+use std::rc::Rc;
+#[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
 
-// b and c linked to a
-// but a was moved to when build list a
+/*
+strong count of a: 1
+link to b, strong count of a: 2
+link to c, strong count of a: 3
+*/
+
 fn main() {
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
-    println!("rc - reference count");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+
+    println!("strong count of a: {}", Rc::strong_count(&a)); // 1
+
+    // create new pointer to a, increase strong refer count
+    let b = Cons(3, Rc::clone(&a));
+    println!("link to b, strong count of a: {}", Rc::strong_count(&a)); // 2
+                                                                        // create new pointer to a
+    let c = Cons(4, Rc::clone(&a));
+    println!("link to c, strong count of a: {}", Rc::strong_count(&a)); // 3
+
+    // a: Cons(5, Cons(10, Nil)), b: Cons(3, Cons(5, Cons(10, Nil))), c: Cons(4, Cons(5, Cons(10, Nil)))
+    println!("a: {:?}, b: {:?}, c: {:?}", a, b, c);
 }
