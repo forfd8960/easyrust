856be58ae3dc6adf884907d24db70904b1fc43bf
forfd8960 <forfd8960@gmail.com>
2023-05-05 23:30:09 +0800

    Default type Parameter

--- examples/advanced-trait/main.rs
+++ examples/advanced-trait/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 struct Counter {
     max: u32,
     current: u32,
@@ -25,9 +27,47 @@ impl Iterator for Counter {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Millimeters(u32);
+#[derive(Debug)]
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + rhs.0 * 1000)
+    }
+}
+
 fn main() {
     let counter = Counter::new(6);
     for v in counter {
         println!("v: {}", v)
     }
+
+    let p1 = Point { x: 1, y: -1 };
+    let p2 = Point { x: 2, y: 0 };
+    // p1 + p2: Point { x: 3, y: -1 }
+    println!("p1 + p2: {:?}", p1 + p2);
+
+    let millmeters = Millimeters(100);
+    let meters = Meters(50);
+    // Millimeters(50100)
+    println!("{:?}", millmeters + meters);
 }
