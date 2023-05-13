4721c013ade1db91363c25efa1bfb673beed548b
forfd8960 <forfd8960@gmail.com>
2023-05-13 09:30:43 +0800

    Paths for Referring to an Item in the Module Tree

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_wait_list() {}
+    pub mod hosting {
+        pub fn add_wait_list() {}
         fn seat_at_table() {}
     }
 
@@ -9,6 +9,37 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+fn delivery_order() {}
+
+mod back_of_hosue {
+    pub struct Breakfirst {
+        pub toast: String,
+        season_fruit: String,
+    }
+
+    impl Breakfirst {
+        pub fn summer(toast: &str) -> Breakfirst {
+            Breakfirst {
+                toast: String::from(toast),
+                season_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        // use super to call parent module function
+        super::delivery_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_wait_list();
+    front_of_house::hosting::add_wait_list();
 }
 
 #[cfg(test)]
