28485673ac94d48d87def6a3e244e15f0459c5df
forfd8960 <forfd8960@gmail.com>
2023-05-28 22:08:51 +0800

    [minigrep] Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,23 @@
 use std::env;
 use std::fs;
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
     let query = &args[1];
     let file_path = &args[2];
 
-    (query, file_path)
+   Config {query: query.to_string(), file_path: file_path.to_string()}
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("query: {}, file_path: {}", query, file_path);
-    let content = fs::read_to_string(file_path).expect("unable to read the fiel");
+    println!("query: {}, file_path: {}", config.query, config.file_path);
+    let content = fs::read_to_string(config.file_path).expect("unable to read the fiel");
     println!("read content\n {content}");
 }
