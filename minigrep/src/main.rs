a3ad965b5b414f820fd81c9a2a9044fafb43a10a
forfd8960 <forfd8960@gmail.com>
2023-05-29 08:38:52 +0800

    [minigrep] Config new

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,17 +5,22 @@ struct Config {
     query: String,
     file_path: String,
 }
+impl Config {
+    fn new(args: &[String]) -> Self {
+        if args.len() < 3 {
+            panic("not enough arguments");
+        }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = &args[1];
-    let file_path = &args[2];
+        let query = &args[1].clone();
+        let file_path = &args[2].clone();
 
-   Config {query: query.to_string(), file_path: file_path.to_string()}
+        Config { query, file_path }
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("query: {}, file_path: {}", config.query, config.file_path);
     let content = fs::read_to_string(config.file_path).expect("unable to read the fiel");
