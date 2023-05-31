0dd1a325d0534741e7ba422ae465919753202ac4
forfd8960 <forfd8960@gmail.com>
2023-05-31 08:57:28 +0800

    [minigrep] Split logic to lib.rs

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,14 +8,22 @@ pub struct Config {
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        
-    }
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-   }
+        let query = &args[1].clone();
+        let file_path = &args[2].clone();
+
+        Ok(Config {
+            query: query.to_string(),
+            file_path: file_path.to_string(),
+        })
+    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    
+    let content = fs::read_to_string(config.file_path)?;
+    println!("read content\n {content}");
+    Ok(())
 }
-
-
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,34 +1,6 @@
+use minigrep::{run, Config};
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = &args[1].clone();
-        let file_path = &args[2].clone();
-
-        Ok(Config {
-            query: query.to_string(),
-            file_path: file_path.to_string(),
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file_path)?;
-    println!("read content\n {content}");
-    Ok(())
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
