77ec37eb235097ff8e4e61b9dcf740913dd97b66
forfd8960 <forfd8960@gmail.com>
2023-05-31 22:27:45 +0800

    [minigrep] Search lines

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,5 +25,35 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
     println!("read content\n {content}");
+
+    for line in search(&config.query, &content) {
+        println!("search result: {line}");
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let mut res: Vec<&'a str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            res.push(line);
+        }
+    }
+   res 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
